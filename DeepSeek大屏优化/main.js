4aae12dfa0b82dc850b6d35a2c074aef33607f8a
zhangxinyu <2361380031@qq.com>
2025-09-03 17:51:22 +0800

    DeepSeek大屏优化

--- DeepSeek大屏优化/main.js
+++ DeepSeek大屏优化/main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DeepSeek大屏优化
 // @namespace    http://tampermonkey.net/
-// @version      2.4.1
+// @version      2.4.2
 // @description  优化DeepSeek页面样式
 // @author       HuSheng
 // @match        https://chat.deepseek.com/**
@@ -22,27 +22,17 @@
     // xpath
     const xpaths_css = [
         {key: '/html/body/div[1]/div/div/div[2]/div[1]/div/div[2]', value: 'display: none', sleep: 0},
-        {key: '/html/body/div[1]/div/div/div[2]/div[1]/div/div[4]/div[1]', value: 'display: none', sleep: 0},
 
         {key: '/html/body/div[1]/div/div/div[2]/div[3]/div/div/div[2]', value: 'width: 100%; max-width: 100%', sleep: 0},
-        {key: '/html/body/div[1]/div/div/div[2]/div[3]/div/div[2]/div/div/div[1]', value: 'width: 100%; max-width: 100%', sleep: 0},
+        {key: '/html/body/div[1]/div/div/div[2]/div[3]/div/div[2]/div/div[2]', value: 'padding: 0', sleep: 0},
         {key: '/html/body/div[1]/div/div/div[2]/div[3]/div/div/div[2]/div[2]', value: 'width: 90%; max-width: 1200px', sleep: 0},
 
-        {key: '/html/body/div[1]/div/div/div[2]/div[3]/div/div[2]/div/div/div[3]/div[1]/div/div/div[1]/div', value: 'min-height: 30px', sleep: 0},
+        {key: '/html/body/div[1]/div/div/div[2]/div[3]/div/div[2]/div/div[2]/div[3]/div[2]/div/div', value: '--line-height: none;', sleep: 0},
         {key: '/html/body/div[1]/div/div/div[2]/div[3]/div/div[2]/div/div/div[3]/div[1]', value: 'width: 100%; max-width: 100%;', sleep: 0},
     ];
 
     // CSS
     const css_css = [
-        // {key: '.ebaea5d2', value: 'display: none', sleep: 0},
-        // {key: '.a1e75851', value: 'display: none', sleep: 0},
-        //
-        // {key: '._9a2f8e4', value: 'width: 100%; max-width: 100%', sleep: 0},
-        // {key: '.dad65929', value: 'width: 100%; max-width: 100%', sleep: 0},
-        // {key: '._9a2f8e4 .aaff8b8f', value: 'width: 80%; max-width: 1000px', sleep: 0},
-        //
-        // {key: '._8f60047 .b13855df', value: 'min-height: 30px', sleep: 0},
-        // {key: '._8f60047 .aaff8b8f', value: 'width: 100%; max-width: 100%', sleep: 0},
     ];
 
 
